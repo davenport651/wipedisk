e7c7440532f107baf9798ca09221a71e17969711
Davenport <41927748+davenport651@users.noreply.github.com>
2022-05-02 10:57:51 -0400

    Update wipedisk.sh
    Finished script; defaults to zero with working yn interface

--- wipedisk.sh
+++ wipedisk.sh
@@ -1,24 +1,49 @@
 #!/bin/bash
 
-#accepts "zero" or "random" as line arguments
+#accepts "zero" or "random" as line arguments; will be used with /dev/ later...
 type=$1
+
+#default to zero if no input
+if [ -z $type ]
+	then type=zero
+	else break
+fi
 
 apt install dcfldd -y
 
 clear
 
 echo Here are the block devices:
-echo $(ls /dev/sd? )
-echo.
-echo Your system disk is: $(lsblk -oMOUNTPOINT,PKNAME -P | grep 'MOUNTPOINT="/"' | sed 's/[0-9]*$//')
-echo.
+echo $(ls /dev/sd* | sed 's/[0-9]*$//' | sort -u )
+echo 
+echo Your system disk is: 
+echo $(lsblk -oMOUNTPOINT,PKNAME -P | grep 'MOUNTPOINT="/"' | sed 's/[0-9]*$//')
+echo 
 #wait for input $disk
+echo Enter device to wipe: 
+read disk
+
 blocksize=$(blockdev --getpbsz $disk)
 
-echo You're about to $type fill $disk with $blocksize bytes per block. Are you sure?
-#choice y/n
+if [ -z $blocksize ]
+then 
+	echo No blocksize. Try to run as root.
+	exit
+fi
 
-dcfldd if=/dev/$type of=$disk bs=$blocksize status=on statusinterval=1024
-sync
+#echo $type $disk $blocksize
+
+echo !!!SANITY CHECK!!!
+echo You are about to $type fill $disk with $blocksize bytes per block.
+read -p "Are you sure? " yn
+case $yn in
+	[nN] ) echo Nothing Happened. && exit;;
+	[yY] ) echo DESTROY ;
+		dcfldd if=/dev/$type of=$disk bs=$blocksize status=on statusinterval=1024;
+    sync;
+    echo It is done.;
+    exit;;
+	* ) echo Something went wrong;;
+esac 
 
 echo It is done.
